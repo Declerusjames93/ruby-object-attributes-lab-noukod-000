9b9c5f983b85962d31583d6d703ad8511255d1f3
James Declerus <james.declerus@esih.edu>
2019-04-10 15:34:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,33 +2,21 @@
 
 
 class Person
-def initialize(fname)
-@fname = fname
-@n_job =n_job
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
 end
-
-def name=(full_name)
-
-@fname = full_name
-end
-
-def name
-puts "#{@fname}"
-end
-def initialize(n_job)
-
-@n_job =n_job
-end
-def job(job_name)
-
-  @n_job = job_name
-end
-
-def job
-  puts "#{@job_name}"
-end
-
-end
-
-beyonce = Person.new
-        beyonce.instance_variable_set(:@job, "Singer")
