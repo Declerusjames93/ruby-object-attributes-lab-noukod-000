1068327de5df382904dc84839f20e3cff7b40d7d
James Declerus <james.declerus@esih.edu>
2019-04-10 15:34:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,21 +2,21 @@
 
 
 class Person
-  
+
   def name=(name)
     @name = name
   end
-  
+
   def name
     @name
   end
 
   def job=(job)
     @job = job
-  end 
+  end
 
-  def job 
+  def job
     @job
-  end 
-  
+  end
+
 end
