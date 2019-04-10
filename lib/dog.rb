55116dc8ea85ebd16f4a0fc06aa08c9e61dd6266
James Declerus <james.declerus@esih.edu>
2019-04-10 15:34:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,19 +2,19 @@
 
 
 class Dog
-  
+
   def name=(dogs_name)
     @name = dogs_name
   end
-  
+
   def name
-    @name 
+    @name
   end
   def breed=(breed)
     @breed = breed
   end
-  
+
   def breed
     @breed
-  end 
+  end
 end
